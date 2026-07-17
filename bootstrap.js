50d1e9fed5028d25d6976dfb84499336f87056c1
mrrian417 <mrrian417@gmail.com>
2026-07-17 21:23:19 +0700

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,6 @@
 // Auto-generated runtime loader for thin Canvas shell.
 (function () {
-  var CDN = "https://cdn.jsdelivr.net/gh/mrrian417/UGC@main?v=20250617";
+  var CDN = "https://cdn.jsdelivr.net/gh/mrrian417/UGC@main";
 
   var _origAdd = document.addEventListener;
   document.addEventListener = function (type, listener, opts) {
